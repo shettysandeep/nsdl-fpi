53520718e4c1be027fdca4ce0861ac2fc0a8f2ea
sandeep <sandeep@arizona.edu>
2025-12-03 10:55:33 +0530

    clean fpi processing file

--- fpi_delimiting_text.py
+++ fpi_delimiting_text.py
@@ -1,10 +1,12 @@
 delimiting_text = {
-        "name": [":\n", "Registration No."],
-        "registration_no": ["Registration No.", "Category of FPI"],
-        "category": ["\nCategory of FPI", "Address"],
-        "fpi_address": ["Address", "SubCategory of FPI"],
-        "sub_category": ["SubCategory of FPI", "Valid upto"],
-        "validity": ["Valid upto", "Country Name"],
-        "country_name": ["Country Name", "Status"],
-        "status_fpi": ["Status", "Name"],
-    }
+    "name": [":\n", "Registration No."],
+    "registration_no": ["Registration No.", "Category of FPI"],
+    "category": ["\nCategory of FPI", "Address"],
+    "fpi_address": ["Address", "SubCategory of FPI"],
+    "sub_category": ["SubCategory of FPI", "Valid upto"],
+    "validity": ["Valid upto", "Country Name"],
+    "country_name": ["Country Name", "Status"],
+    "status_fpi": ["Status", "Name"],
+}
+
+cols_keep = ["SCRIP_NAME", "ISIN", "TR_DATE", "TR_TYPE", "RATE", "QUANTITY", "VALUE"]
--- fpi_processing.py
+++ fpi_processing.py
@@ -4,25 +4,52 @@ import pandas as pd
 from pathlib import Path
 from fpi_delimiting_text import cols_keep
 
-# Combine all the months for year 2024
-fpi_data = Path() / "nsdl_data" / "2024/"
 
-year_2024 = pd.DataFrame()
+class combine_data:
+    def __init__(self, pathfile):
+        self.cols_keep = cols_keep
+        if isinstance(pathfile, Path):
+            self.pathfile = pathfile
+        else:
+            self.pathfile = Path(pathfile)
+        self.file_list = self.extract_files()
 
-for itms in fpi_data.iterdir():
-    print(itms)
-    if itms.with_suffix(".xlsx"):
-        df = pd.read_excel(itms, engine="openpyxl")
-        df.rename(
-            columns={"TR_TYPE(*)": "TR_TYPE", "VALUE (in Rs)": "VALUE"}, inplace=True
-        )
-        df.loc[:, "TR_DATE"] = pd.to_datetime(df["TR_DATE"])
-        df = df[cols_keep]
-        mnth = itms.stem.lower()[:-5]
-        year = itms.stem.lower()[-4:]
-        df["month"] = mnth
-        df["year"] = year
-        year_2024 = pd.concat([year_2024, df])
+    def extract_files(self) -> list:
+        # Combine all the months for year 2024
+        if self.pathfile.is_dir():
+            file_list = [
+                itms for itms in self.pathfile.iterdir() if itms.with_suffix(".xlsx")
+            ]
+            return file_list
+        else:
+            [self.pathfile]
 
-year_2024.to_csv(Path("..") / "fpi_2024.csv")
-year_2024.to_parquet(Path("..") / "fpi_2024.parquet")
+    def clean_combine(self):
+
+        aug_df = pd.DataFrame()
+        for itms in self.file_list:
+            df = pd.read_excel(itms, engine="openpyxl")
+            (
+                df.rename(
+                    columns={"TR_TYPE(*)": "TR_TYPE", "VALUE (in Rs)": "VALUE"},
+                    inplace=True,
+                )
+            )
+            df.loc[:, "TR_DATE"] = pd.to_datetime(df["TR_DATE"])
+            df = df[self.cols_keep]
+            mnth = itms.stem.lower()[:-5]
+            year = itms.stem.lower()[-4:]
+            df["month"] = mnth
+            df["year"] = year
+            aug_df = pd.concat([aug_df, df])
+        return aug_df
+
+
+if __name__ == "__main__":
+
+    testpath = Path() / "nsdl_data" / "2025"
+    file_2025 = combine_data(testpath)
+    combined_2025 = file_2025.clean_combine()
+
+    combined_2025.to_csv(Path("..") / "fpi_2025.csv")
+    # year_2024.to_parquet(Path("..") / "fpi_2024.parquet")
