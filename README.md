1c6798c2e3b349dcd148d7435ccf63ab5d49940e
sandeep <sandeep@arizona.edu>
2025-12-03 13:11:30 +0530

    analysis code started, added Readme

---
+++ README.md
@@ -0,0 +1,2 @@
+# FPIs in India
+This repo extracts, cleans and analyzes the FPI buy/sale data from NSDL. Looking at only secondary market transactions in indian equities. Generating some data drive insights on the transactions.
--- fpi_analysis.py
+++ fpi_analysis.py
@@ -5,39 +5,48 @@ from fpi_delimiting_text import cols_keep
 
 fpi = pd.read_csv("nsdl_data/jan_buy_sell_2025.csv", index_col=0)
 fpi = fpi[cols_keep]
+fpi_dat = fpi[(fpi.TR_TYPE == 1) | (fpi.TR_TYPE == 4)]
 
 
-def print_please(func, rows=10):
-    output = func()
-    if isinstance(output, pd.DataFrame):
-        print(output[:rows])
-    else:
-        print(output)
+def top_5(func):
+    def wrapper(*args, **kwargs):
+        output = func(*args, **kwargs)
+        print(output[:5])
+
+    return wrapper
 
 
-@print_please
-def min_max_price(df, action="buy"):
-    if action == "buy":
-        return (
-            df[df.TR_TYPE == action]
-            .groupby(["SCRIP_NAME", "ISIN"], as_index=True)
-            .agg(
-                {"VALUE": "sum", "RATE": ["min", "max"]}
-                # TOTAL_VALUE = pd.NamedAgg(column="VALUE", aggfunc="sum"),
-                # LOWEST_PRICE =pd.NamedAgg(column="RATE", aggfunc="min"),
-                # WEGHTED_RATE =
-            )
-            .sort_values(by=("VALUE", "sum"), ascending=False)
+@top_5
+def min_max_price(df, action=1):
+    """Returns top 5 bought and sold stocks in a given time period.
+    action = 1 --> buy eq in secondary markets
+    action = 4 --> sell eq in secondary markets
+    """
+
+    return (
+        df[df.TR_TYPE == action]
+        .groupby(["SCRIP_NAME", "ISIN"], as_index=True)
+        .agg(
+            # {"VALUE": "sum", "RATE": ["min", "max"]}
+            TOTAL_VALUE=pd.NamedAgg(column="VALUE", aggfunc="sum"),
+            MIN_PRICE=pd.NamedAgg(column="RATE", aggfunc="min"),
+            MAX_PRICE=pd.NamedAgg(column="RATE", aggfunc="max"),
+            # WEGHTED_RATE =
+            # lambda x: x.sum()
         )
-    if action == "sell":
-        return (
-            df[df.TR_TYPE == action]
-            .groupby(["SCRIP_NAME", "ISIN"], as_index=True)
-            .agg(
-                {"VALUE": "sum", "RATE": ["min", "max"]}
-                # TOTAL_VALUE = pd.NamedAgg(column="VALUE", aggfunc="sum"),
-                # LOWEST_PRICE =pd.NamedAgg(column="RATE", aggfunc="min"),
-                # WEGHTED_RATE =
-            )
-            .sort_values(by=("VALUE", "sum"), ascending=False)
-        )
+        # .sort_values(by=("VALUE", "sum"), ascending=False)
+    )
+
+
+min_max_price(fpi_dat, 1)
+
+""" 
+There is a lot to do here. 
+some isin stocks when it was sold at different times by the FPI.
+let's keep it simple. 
+the duration of sales. how fpis sell lots. 
+sometimes it could be within fpi locations, etc. 
+there is a lot to unpack.
+this will be fun.
+
+"""
