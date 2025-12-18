47aab08eae37d6e8a3571ae6f0970e86a0b1fb18
sandeep <sandeep@arizona.edu>
2025-12-18 11:25:57 +0530

    working plots

--- app.py
+++ app.py
@@ -1,33 +1,47 @@
+from shiny import reactive  # , req
+from shiny.express import input, ui, render
+from shinywidgets import render_plotly
+
+import plotly.express as px
+
 from datetime import datetime
 from faicons import icon_svg
 from pathlib import Path
 
-import plotly.express as px
-from shinywidgets import render_plotly
-
-from shiny import reactive, render, req
-from shiny.express import input, ui
-
-# import fpi_analysis
-
 import pandas as pd
 import calendar
+import pyarrow as pa
+import pyarrow.parquet as pq
+import sys
+
+CRORE = 10000000
+MNTHS = [m.lower() for m in calendar.month_abbr]
 
 pth = Path("fpi_data/")
-datfile = "fpi_2024_25.csv"
+datfile = "fpi_2024_2025.parquet"
+
 # source data for FPI flows - 2024-25
-df = pd.read_csv(pth / datfile)
-df["ISIN"] = df["ISIN"].str.strip()
-df["mf"] = df.ISIN.apply(lambda x: 1 if x[:3] == "INF" else 0)
+df = pq.read_table("fpi_data/fpi_2024_2025.parquet")
+df = df.to_pandas()
+
+print("~~~~\n")
 
 
 # ISIN list
 list_isin = pd.read_csv(pth / "active_CM_DEBT_list.csv")
+STCK_LIST = list_isin[list_isin.instrument_type == "Equity"]["company_name"].to_list()
+
+
+# ~~Transaction type - buy or sell only
+df = df[(df["TR_TYPE"] == 1) | (df["TR_TYPE"] == 4)]  #
+df["TR_TYPE"] = df["TR_TYPE"].astype("category")
+df["TR_TYPE"] = df.TR_TYPE.cat.rename_categories({1: "Buy", 4: "Sell"})
+df["TR_DATE"] = pd.to_datetime(df["TR_DATE"])
 
 
 # ------helper
 def string_to_date(date_str):
-    return datetime.strptime(date_str, "%Y-%m-%d").date()
+    return datetime.strptime(date_str, "%b%Y").date()
 
 
 def filter_by_date(df: pd.DataFrame, date_range: tuple):
@@ -35,9 +49,6 @@ def filter_by_date(df: pd.DataFrame, date_range: tuple):
     dates = pd.to_datetime(df["Date"], format="%Y-%m-%d").dt.date
     return df[(dates >= rng[0]) & (dates <= rng[1])]
 
-
-CRORE = 10000000
-MNTHS = [m.lower() for m in calendar.month_abbr]
 
 # ------
 ICONS = {
@@ -47,15 +58,17 @@ ICONS = {
 # ----- Main Page
 ui.page_opts(title="FPI Monitor - Equity Secondary Markets")  # fillable=True)
 
+# split the page horizontally in 2 parts - first for overall and lower for stock level
+# ~~~~~~~~~~~~~~~~~~~~~~~~
+
 with ui.sidebar(open="desktop"):
     ui.input_selectize("mnth", "Select Month", MNTHS, selected="jan")
     ui.input_selectize("yr", "Select Year", ["2025", "2024"])
     ui.input_slider(
         "usd_inr", "$/INR", min=88.0, max=90.0, value=88.2, step=0.2, pre="$", sep=","
     )
+    ui.input_selectize("equity", "Select equity", STCK_LIST)
 
-
-# print(df_mnth.head())
 
 # with ui.layout_column_wrap():
 with ui.layout_columns(fill=False):
@@ -87,6 +100,7 @@ with ui.layout_columns(fill=False):
 
 with ui.layout_columns(col_widths=[6, 6, 12]):
 
+    # 1) bought top 5
     with ui.card(full_screen=True):
         ui.card_header("Top 5 Stocks Bought (in value) ")
 
@@ -94,12 +108,85 @@ with ui.layout_columns(col_widths=[6, 6, 12]):
         def table():
             return render.DataGrid(top_5())
 
+    # 2) sold top 5
     with ui.card(full_screen=True):
         ui.card_header("Top 5 Sold")
 
         @render.data_frame
         def table1():
             return render.DataGrid(top_5_sold())
+
+    # 3) FPI activity overall
+    with ui.card():
+        ui.card_header("FPI activity in equity markets...")
+
+        @render_plotly
+        def overall_chart():
+            """ """
+
+            dt = df  # for_that_mnth()
+            use_dt = (
+                dt.groupby(["month", "year", "TR_TYPE"], observed=True)
+                .sum("VALUE")
+                .reset_index()
+                .sort_values(by=["month", "year"], ascending=True)
+            )
+            # use_dt["m_y"]=use_dt["TR_DATE"].apply(lambda x: string_to_date(x))
+            use_dt["date"] = pd.to_datetime(
+                use_dt["month"] + use_dt["year"].astype(int).astype(str),
+                format="%b%Y",
+            )  # .dt.strftime("%b-%Y")
+            print(use_dt.head())
+            lineplot = px.bar(
+                data_frame=use_dt,
+                x="date",
+                y="VALUE",
+                color="TR_TYPE",
+                barmode="group",
+                title="FPI activity across months in Secondary markets",
+                labels={"TR_TYPE": "", "m_y": "", "VALUE": "INR"},
+            )
+            return lineplot
+
+
+with ui.layout_columns():
+    # 1) Stock level chart --
+    with ui.card():
+        ui.card_header("FPI activity in stock...")
+
+        @render_plotly
+        def stock_chart():
+            """
+            Stock specific charts of inflow and outflow
+
+            :param stock_name: "Name of stock selected from dropdown.
+            Combine the name and ISIN in a tuple
+            """
+
+            dt = df.copy()  # for_that_mnth()
+            applied_dt = dt[dt["company_name"] == input.equity()]
+            name_scrip = applied_dt.company_name.to_list()[0]
+            # print(name_scrip)
+            use_dt = (
+                applied_dt.groupby(["month", "year", "TR_TYPE"], observed=True)
+                .sum("VALUE")
+                .reset_index()
+                .sort_values(by=["month", "year"], ascending=True)
+            )
+            use_dt["m_y"] = pd.to_datetime(
+                use_dt["month"] + use_dt["year"].astype(int).astype(str),
+                format="%b%Y",
+            )
+            lineplot = px.bar(
+                data_frame=use_dt,
+                x="m_y",
+                y="VALUE",
+                color="TR_TYPE",
+                barmode="group",
+                title=name_scrip,
+                labels={"TR_TYPE": "", "m_y": "", "VALUE": "INR"},
+            )
+            return lineplot
 
 
 # --------------------------------------------------------
@@ -112,16 +199,16 @@ def top_5():
     return (
         df[
             (df["month"] == input.mnth())
-            & (df["TR_TYPE"] == 1)
+            & (df["TR_TYPE"] == "Buy")
             & (df["year"] == int(input.yr()))
         ]
-        .groupby("ISIN")["VALUE"]
+        .groupby("ISIN")["value_crores"]
         .sum()
         .sort_values(ascending=False)
-        .iloc[:10]
+        .iloc[:5]
         .reset_index()
         .merge(list_isin, left_on="ISIN", right_on="ISIN", how="left")[
-            ["company_name", "ISIN"]
+            ["company_name", "ISIN", "value_crores"]
         ]
     )
 
@@ -131,13 +218,13 @@ def top_5_sold():
     return (
         df[
             (df["month"] == input.mnth())
-            & (df["TR_TYPE"] == 4)
+            & (df["TR_TYPE"] == "Sell")
             & (df["year"] == int(input.yr()))
         ]
         .groupby("ISIN")["VALUE"]
         .sum()
         .sort_values(ascending=False)
-        .iloc[:10]
+        .iloc[:5]
         .reset_index()
         .merge(list_isin, left_on="ISIN", right_on="ISIN", how="left")[
             ["company_name", "ISIN"]
@@ -151,7 +238,7 @@ def bght():
     last_value = (
         df[
             (df["month"] == input.mnth())
-            & (df["TR_TYPE"] == 1)
+            & (df["TR_TYPE"] == "Buy")
             & (df["year"] == int(input.yr()))
         ][["VALUE"]]
         .sum()
@@ -168,7 +255,7 @@ def sold():
     last_value = (
         df[
             (df["month"] == input.mnth())
-            & (df["TR_TYPE"] == 4)
+            & (df["TR_TYPE"] == "Sell")
             & (df["year"] == int(input.yr()))
         ][["VALUE"]]
         .sum()
--- fpi_analysis.py
+++ fpi_analysis.py
@@ -2,17 +2,59 @@
 
 import pandas as pd
 from fpi_delimiting_text import cols_keep
+import matplotlib.pyplot as plt
+import plotly.express as px
+import pyarrow.parquet as pq
 
 
-fpi = pd.read_csv("nsdl_data/jan_buy_sell_2025.csv", index_col=0)
-fpi = fpi[cols_keep]
-fpi_dat = fpi[(fpi.TR_TYPE == 1) | (fpi.TR_TYPE == 4)]
-fpi_dat["ISIN"] = fpi_dat["ISIN"].str.strip()
-fpi_dat["mf"] = fpi_dat.ISIN.apply(lambda x: 1 if x[:3] == "INF" else 0)
+#fpi = pd.read_csv("fpi_data/fpi_2024_25.csv")
+fpi = pq.read_table("fpi_data/fpi_2024_2025.parquet",
+                    coerce_int96_timestamp_unit='ms')
+print(fpi)
+fpi = fpi.to_pandas()
 
-print(fpi_dat["ISIN"].unique().shape)
+print(fpi.dtypes)
+fpi_dat = fpi[cols_keep]
+fpi_dat = fpi_dat[
+    (fpi_dat.TR_TYPE == 1) | (fpi_dat.TR_TYPE == 4)
+]  # buy/sale transation
+fpi_dat.loc[:, "ISIN"] = fpi_dat.ISIN.str.strip()
+fpi_dat.loc["mf"] = fpi_dat.ISIN.apply(lambda x: 1 if x[:3] == "INF" else 0)
+# print(fpi_dat[250:26666660]['TR_DATE'])
+# fpi_dat["TR_DATE"]=fpi_dat.TR_DATE.where(fpi_dat["TR_DATE"]=='2025-01-01',
+#                                         '2025-01-01 00:00:00')
+fpi_dat.loc["TR_DATE"] = pd.to_datetime(fpi_dat["TR_DATE"], format="mixed")
+# fpi_dat.loc["month"] = fpi_dat.TR_DATE.dt.month
+print(fpi_dat.head())
+print(fpi_dat.SCRIP_NAME.iloc[11])
+print(fpi_dat.TR_DATE.value_counts())
 
 
+# plot a stock
+
+
+def stock_chart(df, stock_name):
+    """
+    Stock specific charts of inflow and outflow
+
+    :param stock_name: "Name of stock selected from dropdown.
+    Combine the name and ISIN in a tuple
+    """
+    lineplot = px.bar(
+        data_frame=df[df["SCRIP_NAME"] == stock_name],
+        x="TR_DATE",
+        y="VALUE",
+        color="TR_TYPE",
+    )
+    return lineplot
+
+
+# %%
+st = stock_chart(df=fpi_dat, stock_name="ADANI TOTAL GAS LIMITED")
+st.show()
+
+
+'''
 def top_5(func):
     def wrapper(*args, **kwargs):
         output = func(*args, **kwargs)
@@ -32,23 +74,19 @@ def min_max_price(df, action=1):
         df[df.TR_TYPE == action]
         .groupby(["ISIN"], as_index=True)
         .agg(
-            # {"VALUE": "sum", "RATE": ["min", "max"]}
             TOTAL_VALUE=pd.NamedAgg(column="VALUE", aggfunc="sum"),
             MIN_PRICE=pd.NamedAgg(column="RATE", aggfunc="min"),
             MAX_PRICE=pd.NamedAgg(column="RATE", aggfunc="max"),
-            # WEGHTED_RATE =
-            # lambda x: x.sum()
         )
-        # .sort_values(by=("VALUE", "sum"), ascending=False)
     )
 
 
 # top 5 equities and not mutual funds
 fpi_dat_grp = fpi_dat[fpi_dat["mf"] == 0].groupby("ISIN")
-# print(len(fpi_dat_grp.groups.keys()))
 
 
 def wghtd_price(df):
+    # obtain weighted price of sale / purchase
     df["WTD_PRICE"] = (df["RATE"] * (df["QUANTITY"] / df["QUANTITY"].sum())).sum()
     return df
 
@@ -64,7 +102,7 @@ ok1 = fpi_dat_grp["VALUE"].mean().sort_values(ascending=False).iloc[:5].reset_in
 list_isin = pd.read_csv("fpi_data/active_CM_DEBT_list.csv")
 # print(list_isin.columns)
 
-mf_isin = pd.read_csv("fpi_data/mf_isin_list.csv")
+# mf_isin = pd.read_csv("fpi_data/mf_isin_list.csv")
 # print(mf_isin.columns)
 
 
@@ -78,13 +116,16 @@ print(pd.merge(ok1, list_isin, left_on="ISIN", right_on="ISIN", how="left"))
 # lambda x: pd.Series((x["RATE"] * x["QUANTITY"]) / np.sum(x["QUANTITY"]))
 
 
+
 """ 
 There is a lot to do here. 
 some isin stocks when it was sold at different times by the FPI.
-let's keep it simple. 
 the duration of sales. how fpis sell lots. 
 sometimes it could be within fpi locations, etc. 
 there is a lot to unpack.
 this will be fun.
 
 """
+
+"""
+'''
